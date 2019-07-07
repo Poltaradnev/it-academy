cc8335c24ff35eeb69966e8258c1fd12f06f1883
Poltaradnev <poltaradnev@mail.ru>
2019-07-07 21:03:55 +0300

    hometask4

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i > 64; --i)
+            Console.WriteLine((char)i);
+        }
+    }
+}
