d4580e534ae1f3a2fbd2065eeac29f07a0ec7144
Poltaradnev <poltaradnev@mail.ru>
2019-07-21 21:44:03 +0300

    hm_06

---
+++ homewor6_1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace hometask6_1
+{
+    class Program
+    {
+        private static int summing(int x, int y)
+        { return x + y; }
+
+        static void Main(string[] args)
+
+        {
+            const int const1 = 1;
+            Random rand = new Random();
+            int[] random_number = new int[const1];
+            int[] entered_number = new int[const1];
+            int[] total_amount = new int[const1];
+            for (int i = 0; i < const1; i++) random_number[i] = rand.Next(1, 100);            
+            Console.WriteLine("Enter {0} random value:", const1);
+            for (int s = 0; s < const1; s++)
+            {
+                entered_number[s] = Convert.ToInt32(Console.ReadLine());
+                if (s < const1 - 1)
+                { Console.WriteLine(const1 - s - 1); }
+            }         
+
+            for (int i = 0; i < const1; i++)
+            {
+               total_amount[i] = summing(random_number[i], entered_number[i]);
+            }           
+            Console.WriteLine("  \nrandom_number    " + "   entered_number   " + "   Total_amount  ");            
+            for (int s = 0, d = 0, w = 0; s < const1; s++, d++, w++)
+            {
+                Console.Write(random_number[s] + "    \t");
+                Console.Write("            " + entered_number[d] + "\t");
+                Console.WriteLine("                 " + total_amount[w]);
+            }
+
+        }
+
+    }
+
+}
---
+++ homework6_2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+namespace homewor6_2
+
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int w = 3;
+           int[] manualinput = new int[w];    
+
+            Console.WriteLine("Enter {0} times any number:", w - 1);
+            for (int s = 0; s < w - 1; s++)
+            {
+                manualinput[s] = Convert.ToInt32(Console.ReadLine());
+                if (s < w - 2) { Console.WriteLine("One more, please, only {0} insertions left", w - s - 1); }
+            }   
+                      
+            for (int s = 0; s < w; s++)                
+            Console.Write(manualinput[s] + "\n");              
+                       
+            Console.WriteLine("Enter another array value");
+            int value = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(" Cell with which number will replace this cell?\nThe first one is 0 index, remember, please");
+            int position = Convert.ToInt16(Console.ReadLine());            
+            for (int i = 0; i < w; i++)
+            {
+                if (i == position)
+                {
+                    for (int s = w - 1; s > i; s--)
+                    {
+                        manualinput[s] = manualinput[s - 1];
+                    }
+                    manualinput[i] = value;
+                }
+                Console.WriteLine(manualinput[i]);
+            }
+
+        }
+
+    }
+
+}
